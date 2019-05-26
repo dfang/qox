26f8ca9f6c68ca8af14eaefce9bbeb6294e64ee0
fang duan <df1228@gmail.com>
2019-05-26 13:21:21 +0800

    FIX gulp

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,48 +1,53 @@
-'use strict';
+"use strict";
 
-import gulp from 'gulp';
-import babel from 'gulp-babel';
+// import gulp from "gulp";
+// import babel from "gulp-babel";
+var gulp = require("gulp");
+var babel = require("gulp-babel");
 
 var plugins = require("gulp-load-plugins")();
 
 function adminTasks() {
-  var pathto = function (file) {
-        return ('./public/' + file);
-      };
+  var pathto = function(file) {
+    return "./public/" + file;
+  };
   var scripts = {
-        src: pathto('javascripts/*.js'),
-        dest: pathto('dist')
-      };
+    src: pathto("javascripts/*.js"),
+    dest: pathto("dist")
+  };
   var styles = {
-        src: pathto('stylesheets/*.scss'),
-        scss: pathto('stylesheets/**/*.scss'),
-        dest: pathto('dist')
-      }
+    src: pathto("stylesheets/*.scss"),
+    scss: pathto("stylesheets/**/*.scss"),
+    dest: pathto("dist")
+  };
 
-  gulp.task('js', function () {
-    return gulp.src(scripts.src)
-    .pipe(babel())
-    .pipe(plugins.concat('app.js'))
-    .pipe(plugins.uglify())
-    .pipe(gulp.dest(scripts.dest));
+  gulp.task("js", function() {
+    return (
+      gulp
+        .src(scripts.src)
+        .pipe(babel())
+        .pipe(plugins.concat("app.js"))
+        // .pipe(plugins.uglify())
+        .pipe(gulp.dest(scripts.dest))
+    );
   });
 
-  gulp.task('sass', function () {
-    return gulp.src(styles.src)
-    .pipe(plugins.sass())
-    .pipe(plugins.csscomb())
-    .pipe(plugins.cleanCss())
-    .pipe(gulp.dest(styles.dest));
+  gulp.task("sass", function() {
+    return gulp
+      .src(styles.src)
+      .pipe(plugins.sass())
+      .pipe(plugins.csscomb())
+      .pipe(plugins.cleanCss())
+      .pipe(gulp.dest(styles.dest));
   });
 
-  gulp.task('watch', function () {
-    gulp.watch(scripts.src, ['js']);
-    gulp.watch(styles.scss, ['sass']);
+  gulp.task("watch", function() {
+    gulp.watch(scripts.src, ["js"]);
+    gulp.watch(styles.scss, ["sass"]);
   });
 
-  gulp.task('default', ['watch']);
+  gulp.task("default", ["watch"]);
 }
-
 
 // Init
 // -----------------------------------------------------------------------------
