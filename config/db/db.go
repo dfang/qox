672087286a76cd0f8d7258aa91f12413a445081b
fang duan <df1228@gmail.com>
2019-05-29 15:21:27 +0800

    Use zap to log db connection info

--- config/db/db.go
+++ config/db/db.go
@@ -12,7 +12,6 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 
 	"github.com/dfang/qor-demo/config"
-	// "github.com/dfang/qor-demo/config/logger"
 	"github.com/qor/l10n"
 	"github.com/qor/media"
 	"github.com/qor/publish2"
@@ -27,6 +26,9 @@ var DB *gorm.DB
 
 func init() {
 	var err error
+
+	// logger, _ := zap.NewDevelopment()
+	// logger.Info("Hello zap", zap.String("key", "value"), zap.Time("now", time.Now()))
 
 	dbConfig := config.Config.DB
 	Logger, _ := zap.NewDevelopment()
@@ -53,8 +55,8 @@ func init() {
 		DB, err = gorm.Open("mysql", fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?parseTime=True&loc=Local", dbConfig.User, dbConfig.Password, dbConfig.Host, dbConfig.Port, dbConfig.Name))
 		// DB = DB.Set("gorm:table_options", "CHARSET=utf8")
 	} else if config.Config.DB.Adapter == "postgres" {
-		zap.S().Debug(fmt.Sprintf("postgres://%v:%v@%v/%v?sslmode=disable", dbConfig.User, dbConfig.Password, dbConfig.Host, dbConfig.Name))
-		DB, err = gorm.Open("postgres", fmt.Sprintf("postgres://%v:%v@%v/%v?sslmode=disable", dbConfig.User, dbConfig.Password, dbConfig.Host, dbConfig.Name))
+		zap.S().Debug(fmt.Sprintf("postgres://%v:%v@%v:%v/%v?sslmode=disable", dbConfig.User, dbConfig.Password, dbConfig.Host, dbConfig.Port, dbConfig.Name))
+		DB, err = gorm.Open("postgres", fmt.Sprintf("postgres://%v:%v@%v:%v/%v?sslmode=disable", dbConfig.User, dbConfig.Password, dbConfig.Host, dbConfig.Port, dbConfig.Name))
 	} else if config.Config.DB.Adapter == "sqlite" {
 		zap.S().Debug(fmt.Sprintf("%v/%v", os.TempDir(), dbConfig.Name))
 		DB, err = gorm.Open("sqlite3", fmt.Sprintf("%v/%v", os.TempDir(), dbConfig.Name))
