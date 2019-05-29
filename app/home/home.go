597da4a6dce2e2a611848b799853a9a8e979c2c4
fang duan <df1228@gmail.com>
2019-05-29 15:42:37 +0800

    Controller read bindatafs

--- app/home/home.go
+++ app/home/home.go
@@ -2,6 +2,7 @@ package home
 
 import (
 	"github.com/qor/application"
+	// "github.com/dfang/qor-demo/config/bindatafas"
 	"github.com/dfang/qor-demo/utils/funcmapmaker"
 	"github.com/qor/render"
 )
@@ -27,6 +28,7 @@ type Config struct {
 // ConfigureApplication configure application
 func (App) ConfigureApplication(application *application.Application) {
 	controller := &Controller{View: render.New(&render.Config{AssetFileSystem: application.AssetFS.NameSpace("home")}, "app/home/views")}
+	// controller := &Controller{ View: render.New( &render.Config{ AssetFileSystem: bindatafs.AssetFS.NameSpace("home"), }),
 
 	funcmapmaker.AddFuncMapMaker(controller.View)
 	application.Router.Get("/", controller.Index)
