82120390e568b08d99d1d7eda3facbf8c2d3fb7c
fang duan <df1228@gmail.com>
2019-08-16 13:06:43 +0800

    Add health_check

--- main.go
+++ main.go
@@ -32,6 +32,7 @@ import (
 	"github.com/dfang/qor-demo/utils/funcmapmaker"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/heptiolabs/healthcheck"
 	"github.com/qor/admin"
 	"github.com/qor/application"
 	"github.com/qor/publish2"
@@ -66,6 +67,18 @@ func main() {
 		migrations.Migrate()
 		os.Exit(0)
 	}
+
+	health := healthcheck.NewHandler()
+	// Our app is not happy if we've got more than 100 goroutines running.
+	health.AddLivenessCheck("goroutine-threshold", healthcheck.GoroutineCountCheck(100))
+	// Our app is not ready if we can't resolve our upstream dependency in DNS.
+	// health.AddReadinessCheck(
+	// 	"upstream-dep-dns",
+	// 	healthcheck.DNSResolveCheck("upstream.example.com", 50*time.Millisecond))
+
+	// Our app is not ready if we can't connect to our database (`var db *sql.DB`) in <1s.
+	health.AddReadinessCheck("database", healthcheck.DatabasePingCheck(db.DB.DB(), 5*time.Second))
+	go http.ListenAndServe("0.0.0.0:8086", health)
 
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 	log.Logger = log.With().Caller().Logger()
