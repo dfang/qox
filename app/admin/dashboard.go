bd4963092e6616dcd383de8f2a2fc4ab32120906
fang duan <df1228@gmail.com>
2019-08-21 17:55:16 +0800

    Update dashboard page

--- app/admin/dashboard.go
+++ app/admin/dashboard.go
@@ -2,7 +2,7 @@ package admin
 
 import (
 	"encoding/json"
-	"time"
+	"strings"
 
 	"github.com/dfang/qor-demo/config/db"
 	"github.com/jinzhu/now"
@@ -11,13 +11,13 @@ import (
 
 type Chart struct {
 	Total string
-	Date  time.Time
+	Date  string
 }
 
 /*
 date format 2015-01-23
 */
-func GetChartData(table, start, end string) (res []Chart) {
+func GetChartDataByDay(table, start, end string) (res []Chart) {
 	startdate, err := now.Parse(start)
 	if err != nil {
 		return
@@ -39,6 +39,33 @@ func GetChartData(table, start, end string) (res []Chart) {
 	return
 }
 
+func GetChartDataByMonth(table, start, end string) (res []Chart) {
+	startdate, err := now.Parse(start)
+	if err != nil {
+		return
+	}
+
+	enddate, err := now.Parse(end)
+	if err != nil || enddate.UnixNano() < startdate.UnixNano() {
+		panic("endDate must > startDate")
+	}
+
+	// select
+	//   to_char(date_trunc('month', created_at), 'YYYY-MM'),
+	//   count(1)
+	// from orders
+	// group by 1
+	// order by 1;
+
+	db.DB.Table(table).
+		Where("created_at > ? AND created_at < ?", startdate, enddate).
+		Select("to_char(date_trunc('month', created_at), 'YYYY-MM') as date, count(1) as total").
+		Group("date").
+		Order("date").
+		Scan(&res)
+	return
+}
+
 type Charts struct {
 	Orders []Chart
 	Users  []Chart
@@ -49,8 +76,14 @@ func ReportsDataHandler(context *admin.Context) {
 	startDate := context.Request.URL.Query().Get("startDate")
 	endDate := context.Request.URL.Query().Get("endDate")
 
-	charts.Orders = GetChartData("orders", startDate, endDate)
-	charts.Users = GetChartData("users", startDate, endDate)
+	// startDate -> 2019-08-01 按天
+	// startDate -> 2019-08 按月
+	if len(strings.Split(startDate, "-")) == 3 {
+		charts.Orders = GetChartDataByDay("orders", startDate, endDate)
+		// charts.Users = GetChartDataByDay("users", startDate, endDate)
+	} else {
+		charts.Orders = GetChartDataByMonth("orders", startDate, endDate)
+	}
 
 	b, _ := json.Marshal(charts)
 	context.Writer.Write(b)
