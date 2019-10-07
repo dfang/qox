d4b42cd7666bcd25627e44d8cf5d148378d0f67f
fang duan <df1228@gmail.com>
2019-10-07 13:28:22 +0800

    MISC

---
+++ models/aftersales/settlement.go
@@ -0,0 +1,21 @@
+package aftersales
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+// Settlement AfterSale 售后厂家管理
+type Settlement struct {
+	gorm.Model
+
+	ManufacturerName 	string
+
+	UserID *uint
+	User   users.User
+
+	// deposit 存入（完成服务）
+	// withdraw 提现 ()
+	// Direction string
+
+	Fee float32
+}
