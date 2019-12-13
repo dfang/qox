fa23ad837f6659a766bc8672545c91c3c3dd3352
fang duan <df1228@gmail.com>
2019-12-13 10:16:40 +0800

    Refactor main.go

--- main.go
+++ main.go
@@ -52,10 +52,254 @@ import (
 const version = "0.0.1" // must follow semver spec, https://github.com/motemen/gobump
 
 var (
-	Router      *chi.Mux
-	Admin       *admin.Admin
+	// Router Chi Router
+	Router *chi.Mux
+	// Admin Qor Admin (后台管理页面)
+	Admin *admin.Admin
+	// Application Qor Application (前端)
 	Application *application.Application
 )
+
+func checkAvailableEnvs() {
+	envs := []string{
+		"QOR_ENV",
+		"DEBUG",
+		"HTTPS",
+		"DOMAIN",
+		"PORT",
+		"DBAdapter",
+		"DBName",
+		"DBPort",
+		"DBHost",
+		"DBUser",
+		"DBPassword",
+		"REDIS_HOST",
+		"REDIS_PORT",
+		"DEMO_MODE",
+	}
+
+	for _, e := range envs {
+		if os.Getenv(e) != "" {
+			fmt.Printf("\tfound env var %s=%s\n", e, os.Getenv(e))
+		}
+	}
+}
+
+func setupLogLevel(debug bool) {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	log.Logger = log.With().Caller().Logger()
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	if debug {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
+}
+
+func initialzeConfigs() {
+	// config.Initialize()
+	// db.Initialize()
+	i18n.Initialize()
+	auth.Initialize()
+}
+
+func setupMiddlewaresAndRoutes() {
+	Router = chi.NewRouter()
+	Admin = admin.New(&admin.AdminConfig{
+		SiteName: "QOR DEMO",
+		Auth:     auth.AdminAuth{},
+		DB:       db.DB.Set(publish2.VisibleMode, publish2.ModeOff).Set(publish2.ScheduleMode, publish2.ModeOff),
+	})
+	Application = application.New(&application.Config{
+		Router: Router,
+		Admin:  Admin,
+		DB:     Admin.DB,
+	})
+
+	funcmapmaker.AddFuncMapMaker(auth.Auth.Config.Render)
+
+	// Health check for k8s pod
+	Router.Use(middleware.Heartbeat("/health"))
+	// TODO: implement Graceful shutdown
+	// https://medium.com/over-engineering/graceful-shutdown-with-go-http-servers-and-kubernetes-rolling-updates-6697e7db17cf
+
+	// https://github.com/qor/qor-example/commit/06835622f5feeeb90aee4f02574abbae29e40e10
+	Router.Use(func(handler http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			req.Header.Del("Authorization")
+			handler.ServeHTTP(w, req)
+		})
+	})
+
+	Router.Use(middleware.RealIP)
+	Router.Use(middleware.RequestID)
+	Router.Use(middleware.Logger)
+	Router.Use(middleware.Recoverer)
+	Router.Use(middleware.Timeout(3 * time.Second))
+	// Router.Use(middleware.WithValue("k", "v"))
+	// Router.Use(middleware.NoCache)
+	// Router.Use(middleware.StripSlashes)
+
+	// Use default options
+	Router.Use(cors.Default().Handler)
+
+	Router.Use(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			var (
+				tx         = db.DB
+				qorContext = &qor.Context{Request: req, Writer: w}
+			)
+
+			if locale := utils.GetLocale(qorContext); locale != "" {
+				tx = tx.Set("l10n:locale", locale)
+			} else {
+				// set default locale to zh-CN
+				// tx = tx.Set("l10n:locale", "zh-CN")
+				expire := time.Now().AddDate(0, 1, 0) // one month later
+				cookie := http.Cookie{Name: "locale", Value: "zh-CN", Path: "/", Expires: expire, MaxAge: 90000}
+				http.SetCookie(w, &cookie)
+			}
+
+			ctx := context.WithValue(req.Context(), utils.ContextDBName, publish2.PreviewByDB(tx, qorContext))
+			next.ServeHTTP(w, req.WithContext(ctx))
+		})
+	})
+
+	if os.Getenv("DEBUG") == "true" {
+		Router.Use(func(next http.Handler) http.Handler {
+			return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+				DumpHTTPRequest(req)
+				next.ServeHTTP(w, req)
+			})
+		})
+	}
+
+	Application.Use(aftersale.NewWithDefault())
+	Application.Use(api.New(&api.Config{}))
+	Application.Use(adminapp.New(&adminapp.Config{}))
+	// Application.Use(home.New(&home.Config{}))
+	Application.Use(account.NewWithDefault())
+	Application.Use(home.NewWithDefault())
+	Application.Use(products.NewWithDefault())
+	Application.Use(orders.NewWithDefault())
+	// Application.Use(pages.NewWithDefault())
+	Application.Use(stores.NewWithDefault())
+	Application.Use(reports.NewWithDefault())
+	// Application.Use(enterprise.New(&enterprise.Config{}))
+
+	// views := []string{
+	// 	"app/home/views",
+	// 	"app/account/views",
+	// 	"app/order/views",
+	// 	"app/page/views",
+	// 	"app/products/views",
+	// 	"app/views",
+	// }
+
+	// for _, v := range views {
+	// 	// bindatafs.AssetFS.NameSpace("views").RegisterPath(filepath.Join(config.Root, v))
+	// 	bindatafs.AssetFS.RegisterPath(filepath.Join(config.Root, v))
+	// }
+
+	Router.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("PONG"))
+	})
+
+	// fs := http.FileServer(http.Dir("public"))
+	// Router.Handle("/", fs)
+
+	// http.Handle("/", http.StripPrefix("/", fs))
+	// Router.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// Router.Handle("/static/", http.RedirectHandler("http://baidu.com", 303))
+	// Router.Handle("/public/", http.StripPrefix("/public/", fs))
+
+	// workDir, _ := os.Getwd()
+	// filesDir := filepath.Join(workDir, "public")
+	// FileServer(Router, "/", http.Dir(filesDir))
+
+	// 用NotFound 来替代前面的三句
+	// Serve public 文件夹里的 favicon.ico 等文件
+	// 确保 /favicon.ico 等能直接访问
+	// 注意 NotFound 和 pages 模块冲突，所以注释了pages
+	// 因为貌似开启了pages模块 永远不会执行到NotFound里来
+	Router.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		// w.WriteHeader(404)
+		// w.Write([]byte(r.URL.Path))
+		// w.Write([]byte("nothing here"))
+		log.Debug().Msg("request path: " + r.URL.Path)
+		// workDir, _ := os.Getwd()
+		// filesDir := filepath.Join(workDir, "public")
+		fs := http.FileServer(http.Dir("./public"))
+		fs.ServeHTTP(w, r)
+		// http.ServeFile(w, r, "./public/favicon.ico")
+	})
+}
+
+// DumpHTTPRequest for debugging
+func DumpHTTPRequest(r *http.Request) {
+	output, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		fmt.Println("Error when dumping request:", err)
+		return
+	}
+	fmt.Println(string(output))
+}
+
+// FileServer conveniently sets up a http.FileServer handler to serve
+// static files from a http.FileSystem.
+// https://github.com/go-chi/chi/blob/master/_examples/fileserver/main.go
+func FileServer(r chi.Router, path string, root http.FileSystem) {
+	if strings.ContainsAny(path, "{}*") {
+		panic("FileServer does not permit URL parameters.")
+	}
+
+	// https://stackoverflow.com/questions/27945310/why-do-i-need-to-use-http-stripprefix-to-access-my-static-files
+	fs := http.StripPrefix(path, http.FileServer(root))
+
+	// fmt.Println("fuck")
+	// fmt.Println(path)
+	// fmt.Println(len(path) - 1)
+	// fmt.Println(path[len(path)-1])
+
+	if path != "/" && path[len(path)-1] != '/' {
+		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
+		path += "/"
+	}
+	path += "*"
+
+	r.Get(path, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		log.Debug().Msg("FALLBACK")
+
+		fmt.Println("fallback to file server")
+		fmt.Println(path)
+
+		fs.ServeHTTP(w, r)
+	}))
+}
+
+func listenAndServe() {
+	if config.Config.HTTPS {
+		certManager := autocert.Manager{
+			Prompt: autocert.AcceptTOS,
+			Cache:  autocert.DirCache("cert-cache"),
+			// Put your domain here:
+			HostPolicy: autocert.HostWhitelist(os.Getenv("DOMAIN")),
+		}
+		server := &http.Server{
+			Addr:    ":443",
+			Handler: Router,
+			TLSConfig: &tls.Config{
+				GetCertificate: certManager.GetCertificate,
+			},
+		}
+		go http.ListenAndServe(fmt.Sprintf(":%d", config.Config.Port), certManager.HTTPHandler(nil))
+		server.ListenAndServeTLS("", "")
+	} else {
+		if err := http.ListenAndServe(fmt.Sprintf(":%d", config.Config.Port), Application.NewServeMux()); err != nil {
+			panic(err)
+		}
+	}
+}
 
 func main() {
 	cmdLine := flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
@@ -140,252 +384,5 @@ func main() {
 	fmt.Printf("Startup took %s\n", elapsed)
 
 	fmt.Printf("Listening on: %v\n\n", config.Config.Port)
-	if config.Config.HTTPS {
-		certManager := autocert.Manager{
-			Prompt: autocert.AcceptTOS,
-			Cache:  autocert.DirCache("cert-cache"),
-			// Put your domain here:
-			HostPolicy: autocert.HostWhitelist(os.Getenv("DOMAIN")),
-		}
-		server := &http.Server{
-			Addr:    ":443",
-			Handler: Router,
-			TLSConfig: &tls.Config{
-				GetCertificate: certManager.GetCertificate,
-			},
-		}
-		go http.ListenAndServe(fmt.Sprintf(":%d", config.Config.Port), certManager.HTTPHandler(nil))
-		server.ListenAndServeTLS("", "")
-	} else {
-		if err := http.ListenAndServe(fmt.Sprintf(":%d", config.Config.Port), Application.NewServeMux()); err != nil {
-			panic(err)
-		}
-	}
-}
-
-func checkAvailableEnvs() {
-	envs := []string{
-		"QOR_ENV",
-		"DEBUG",
-		"HTTPS",
-		"DOMAIN",
-		"PORT",
-		"DBAdapter",
-		"DBName",
-		"DBPort",
-		"DBHost",
-		"DBUser",
-		"DBPassword",
-		"REDIS_HOST",
-		"REDIS_PORT",
-		"DEMO_MODE",
-	}
-
-	for _, e := range envs {
-		if os.Getenv(e) != "" {
-			fmt.Printf("\tfound env var %s=%s\n", e, os.Getenv(e))
-		}
-	}
-}
-
-func setupLogLevel(debug bool) {
-	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
-	log.Logger = log.With().Caller().Logger()
-	zerolog.SetGlobalLevel(zerolog.InfoLevel)
-	if debug {
-		zerolog.SetGlobalLevel(zerolog.DebugLevel)
-	}
-}
-
-func initialzeConfigs() {
-	// config.Initialize()
-	// db.Initialize()
-	i18n.Initialize()
-	auth.Initialize()
-}
-
-func setupMiddlewaresAndRoutes() {
-	Router = chi.NewRouter()
-	Admin = admin.New(&admin.AdminConfig{
-		SiteName: "QOR DEMO",
-		Auth:     auth.AdminAuth{},
-		DB:       db.DB.Set(publish2.VisibleMode, publish2.ModeOff).Set(publish2.ScheduleMode, publish2.ModeOff),
-	})
-	Application = application.New(&application.Config{
-		Router: Router,
-		Admin:  Admin,
-		DB:     Admin.DB,
-	})
-
-	funcmapmaker.AddFuncMapMaker(auth.Auth.Config.Render)
-
-	// Health check for k8s pod
-	Router.Use(middleware.Heartbeat("/health"))
-	// TODO: implement Graceful shutdown
-	// https://medium.com/over-engineering/graceful-shutdown-with-go-http-servers-and-kubernetes-rolling-updates-6697e7db17cf
-
-	// https://github.com/qor/qor-example/commit/06835622f5feeeb90aee4f02574abbae29e40e10
-	Router.Use(func(handler http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-			req.Header.Del("Authorization")
-			handler.ServeHTTP(w, req)
-		})
-	})
-
-	Router.Use(middleware.RealIP)
-	Router.Use(middleware.RequestID)
-	Router.Use(middleware.Logger)
-	Router.Use(middleware.Recoverer)
-	Router.Use(middleware.Timeout(3 * time.Second))
-	// Router.Use(middleware.WithValue("k", "v"))
-	// Router.Use(middleware.StripSlashes)
-
-	// Use default options
-	Router.Use(cors.Default().Handler)
-
-	Router.Use(func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-			var (
-				tx         = db.DB
-				qorContext = &qor.Context{Request: req, Writer: w}
-			)
-
-			if locale := utils.GetLocale(qorContext); locale != "" {
-				tx = tx.Set("l10n:locale", locale)
-			} else {
-				// set default locale to zh-CN
-				// tx = tx.Set("l10n:locale", "zh-CN")
-				expire := time.Now().AddDate(0, 1, 0) // one month later
-				cookie := http.Cookie{Name: "locale", Value: "zh-CN", Path: "/", Expires: expire, MaxAge: 90000}
-				http.SetCookie(w, &cookie)
-			}
-
-			ctx := context.WithValue(req.Context(), utils.ContextDBName, publish2.PreviewByDB(tx, qorContext))
-			next.ServeHTTP(w, req.WithContext(ctx))
-		})
-	})
-
-	if os.Getenv("DEBUG") == "true" {
-		Router.Use(func(next http.Handler) http.Handler {
-			return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-				DumpHTTPRequest(req)
-				next.ServeHTTP(w, req)
-			})
-		})
-	}
-
-	// Router.NotFound(func(w http.ResponseWriter, r *http.Request) {
-	// 	// w.WriteHeader(404)
-	// 	// w.Write([]byte(r.URL.Path))
-	// 	// w.Write([]byte("nothing here"))
-	// 	// workDir, _ := os.Getwd()
-	// 	// filesDir := filepath.Join(workDir, "public")
-	// 	// fs := http.FileServer(http.Dir("public"))
-	// 	// fs.ServeHTTP(w, r)
-	// 	http.ServeFile(w, r, "favicon.ico")
-	// })
-
-	Application.Use(aftersale.NewWithDefault())
-	Application.Use(api.New(&api.Config{}))
-	Application.Use(adminapp.New(&adminapp.Config{}))
-	// Application.Use(home.New(&home.Config{}))
-	Application.Use(account.NewWithDefault())
-	Application.Use(home.NewWithDefault())
-	Application.Use(products.NewWithDefault())
-	Application.Use(orders.NewWithDefault())
-	// Application.Use(pages.NewWithDefault())
-	Application.Use(stores.NewWithDefault())
-	Application.Use(reports.NewWithDefault())
-	// Application.Use(enterprise.New(&enterprise.Config{}))
-
-	// views := []string{
-	// 	"app/home/views",
-	// 	"app/account/views",
-	// 	"app/order/views",
-	// 	"app/page/views",
-	// 	"app/products/views",
-	// 	"app/views",
-	// }
-
-	// for _, v := range views {
-	// 	// bindatafs.AssetFS.NameSpace("views").RegisterPath(filepath.Join(config.Root, v))
-	// 	bindatafs.AssetFS.RegisterPath(filepath.Join(config.Root, v))
-	// }
-
-	Router.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("PONG"))
-	})
-
-	// fs := http.FileServer(http.Dir("public"))
-	// Router.Handle("/", fs)
-
-	// http.Handle("/", http.StripPrefix("/", fs))
-	// Router.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	// Router.Handle("/static/", http.RedirectHandler("http://baidu.com", 303))
-	// Router.Handle("/public/", http.StripPrefix("/public/", fs))
-
-	// workDir, _ := os.Getwd()
-	// filesDir := filepath.Join(workDir, "public")
-	// FileServer(Router, "/", http.Dir(filesDir))
-
-	// 用NotFound 来替代前面的三句
-	// Serve public 文件夹里的 favicon.ico 等文件
-	// 确保 /favicon.ico 等能直接访问
-	Router.NotFound(func(w http.ResponseWriter, r *http.Request) {
-		// w.WriteHeader(404)
-		// w.Write([]byte(r.URL.Path))
-		// w.Write([]byte("nothing here"))
-
-		log.Debug().Msg("request path: " + r.URL.Path)
-
-		// workDir, _ := os.Getwd()
-		// filesDir := filepath.Join(workDir, "public")
-		fs := http.FileServer(http.Dir("./public"))
-		fs.ServeHTTP(w, r)
-		// http.ServeFile(w, r, "./public/favicon.ico")
-	})
-}
-
-// DumpHTTPRequest for debugging
-func DumpHTTPRequest(r *http.Request) {
-	output, err := httputil.DumpRequest(r, true)
-	if err != nil {
-		fmt.Println("Error when dumping request:", err)
-		return
-	}
-	fmt.Println(string(output))
-}
-
-// FileServer conveniently sets up a http.FileServer handler to serve
-// static files from a http.FileSystem.
-// https://github.com/go-chi/chi/blob/master/_examples/fileserver/main.go
-func FileServer(r chi.Router, path string, root http.FileSystem) {
-	if strings.ContainsAny(path, "{}*") {
-		panic("FileServer does not permit URL parameters.")
-	}
-
-	// https://stackoverflow.com/questions/27945310/why-do-i-need-to-use-http-stripprefix-to-access-my-static-files
-	fs := http.StripPrefix(path, http.FileServer(root))
-
-	// fmt.Println("fuck")
-	// fmt.Println(path)
-	// fmt.Println(len(path) - 1)
-	// fmt.Println(path[len(path)-1])
-
-	if path != "/" && path[len(path)-1] != '/' {
-		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
-		path += "/"
-	}
-	path += "*"
-
-	r.Get(path, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
-		log.Debug().Msg("FALLBACK")
-
-		fmt.Println("fallback to file server")
-		fmt.Println(path)
-
-		fs.ServeHTTP(w, r)
-	}))
+	listenAndServe()
 }
